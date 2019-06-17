2b3e2d51c0f4d0958a566582586756296b7893df
Simon Guyennet <simon@guyennet.fr>
2019-06-17 15:56:34 +0200

    Fix issue with Linux sed of the admin.conf file

--- vsphere-kubespray.tf
+++ vsphere-kubespray.tf
@@ -397,7 +397,7 @@ resource "null_resource" "kubectl_configuration" {
   }
 
   provisioner "local-exec" {
-    command = "sed 's/lb-apiserver.kubernetes.local/${var.vm_haproxy_vip}/g' config/admin.conf | tee config/admin.conf"
+    command = "sed 's/lb-apiserver.kubernetes.local/${var.vm_haproxy_vip}/g' config/admin.conf | tee config/admin.conf.new && mv config/admin.conf.new config/admin.conf && chmod 700 config/admin.conf"
   }
 
   provisioner "local-exec" {
